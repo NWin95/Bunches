e0408edd26902d1ba2a353571d3803ec498ecb53
NWin95 <winsor.nick.cs@gmail.com>
2015-12-29 23:00:43 -0600

    Buildings And Clouds

--- BunchesProject/Assets/Scripts/ResetColl.cs
+++ BunchesProject/Assets/Scripts/ResetColl.cs
@@ -4,9 +4,24 @@ using UnityEngine.SceneManagement;
 
 public class ResetColl : MonoBehaviour {
 
+    public int enemiesKilled;
+
 	void OnTriggerEnter (Collider coll)
     {
         if (coll.name == "Player")
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        if (coll.tag == "Enemy")
+        {
+            StartCoroutine(EnemyKilled(coll.gameObject));
+        }
+    }
+
+    IEnumerator EnemyKilled (GameObject obj)
+    {
+        enemiesKilled++;
+        obj.transform.GetChild(0).GetComponent<Animator>().SetTrigger("FallTrigger");
+
+        yield return new WaitForSeconds(2.5f);
+        Destroy(obj);
     }
 }
