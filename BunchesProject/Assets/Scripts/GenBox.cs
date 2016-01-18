670f835f7c07ad4cb6cd74ff0682238ad238fcbe
NWin95 <winsor.nick.cs@gmail.com>
2016-01-18 06:21:33 -0600

    Runner Mode Working
    get reset button and fall
    fix the blinking bug

---
+++ BunchesProject/Assets/Scripts/GenBox.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenBox : MonoBehaviour {
+
+    public GameObject enemyObj;
+    public Vector3 dimmensions;
+    public Vector3[] startPoints;
+    public Vector3[] endPoints;
+    public Vector3[] spawnPoints;
+    public float[] spawnRots;
+
+    public bool spawnedBox;
+
+    void Start ()
+    {
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        int spawnInt = 0;
+
+        foreach (Vector3 point in spawnPoints)
+        {
+            Vector3 spawnPos = spawnPoints[spawnInt];
+            spawnPos = transform.TransformPoint(spawnPos);
+
+            Vector3 spawnEuler = Vector3.zero;
+            spawnEuler.y = spawnRots[spawnInt];
+            Quaternion spawnRot = Quaternion.Euler(spawnEuler);
+
+            GameObject spawnedEnemy = GameObject.Instantiate(enemyObj, spawnPos, spawnRot) as GameObject;
+            spawnInt++;
+        }
+    }
+
+    void OnTriggerEnter (Collider coll)
+    {
+        if (coll.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            if (!spawnedBox)
+            {
+                spawnedBox = true;
+                transform.parent.GetComponent<GenBoxSpawner>().SpawnGenBox(/*gameObject*/);
+                //Debug.Log("Collided");
+            }
+        }
+    }
+}
---
+++ BunchesProject/Assets/Scripts/GenBoxSpawner.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenBoxSpawner : MonoBehaviour {
+
+    public GameObject[] boxes;
+
+    Vector3 basePoint = new Vector3(0, -1000, -1000);
+
+    void Start ()
+    {
+        SpawnGenBox();
+        SpawnGenBox();
+        SpawnGenBox();
+        SpawnGenBox();
+    }
+
+    public void SpawnGenBox (/* GameObject box */)
+    {
+        if (transform.childCount > 0)
+        {
+            Transform lastChild = transform.GetChild(transform.childCount - 1);
+            //Debug.Log(lastChild.name);
+
+            int randInt = Random.Range(0, boxes.Length);
+            Debug.Log(randInt);
+
+            GameObject spawnedBox = GameObject.Instantiate(boxes[randInt], basePoint, Quaternion.identity) as GameObject;
+            spawnedBox.transform.parent = transform;
+
+            Vector3 spawnPos = lastChild.position + lastChild.GetComponent<GenBox>().endPoints[0];
+            spawnPos -= spawnedBox.GetComponent<GenBox>().startPoints[0];
+
+            spawnedBox.transform.position = spawnPos;
+        }
+        else
+        {
+            int randInt = Random.Range(0, boxes.Length);
+            //Debug.Log(randInt);
+
+            GameObject spawnedBox = GameObject.Instantiate(boxes[randInt], basePoint, Quaternion.identity) as GameObject;
+            spawnedBox.transform.parent = transform;
+
+            Vector3 spawnPos = new Vector3(0, 0, 6);
+            spawnPos -= spawnedBox.GetComponent<GenBox>().startPoints[0];
+
+            spawnedBox.transform.position = spawnPos;
+        }
+    }
+}
